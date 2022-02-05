030076715545f5dec16bd422fb2b17d63c8ac301
Adeyemi <addegbenga@gmail.com>
2022-02-05 23:15:59 +0100

    fav

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,9 @@ import "./style.css";
 
 export default function Hero() {
   return (
-    <div className="px-3  lg:mt-16 lg:flex lg:items-center justify-between container mx-auto lg:gap-12">
-      <div className="my-32 mb-0 lg:max-w-3xl">
-        <h1 className="text-4xl leading-[2.7rem] lg:leading-[6rem] lg:text-[5.1rem] lg:fontbold font-extrabold">
+    <div className="px-3  mt-36 lg:mt-16 lg:flex lg:items-center justify-between container mx-auto lg:gap-12">
+      <div className=" lg:my-32 mb-0 lg:max-w-3xl">
+        <h1 className="text-[2.5rem] leading-[3.3rem] lg:leading-[6rem] lg:text-[5.1rem] lg:fontbold font-extrabold">
           <span className="text-black headline">I design products</span> that
           delight and inspire people.{" "}
         </h1>
@@ -15,7 +15,7 @@ export default function Hero() {
           user-frindly interfaces for fast growing and established companies
         </p>
         <div className="flex gap-2 ">
-          <button className="bg-black shadow gap-1 flex items-center rounded text-white lg:p-4  lg:px-10 p-2 px-10">
+          <button className="bg-black shadow gap-1 flex items-center rounded text-white lg:p-4  lg:px-10 p-4 px-10">
             <span>Book a call</span>
           </button>
           <button className="border gap-2 shadow flex items-center rounded lg:p-4 lg:px-10 text-black p-2 px-6">
@@ -24,11 +24,11 @@ export default function Hero() {
           </button>
         </div>
       </div>
-      <div className="flex lg:hidden my-14 justify-center">
+      <div className="flex  lg:hidden my-14 justify-center">
         <img className="w-2/3" src="/assets/dope9.webp" alt="png" />
       </div>
-      <div className="flex hidden lg:block my-14 justify-center">
-        <img className="w-full " src="/assets/hero.png" alt="png" />
+      <div className="flex hidden lg:block mb-20  lg:my-14 justify-center">
+        <img className="w-1/2 lg:w-full " src="/assets/hero.png" alt="png" />
       </div>
     </div>
   );
--- src/components/Navabar.js
+++ src/components/Navabar.js
@@ -4,9 +4,9 @@ export default function Navabar() {
   return (
     <nav className="w-full">
       <div className="flex px-3 py-6  border-b   w-full left-0 bg-white fixed z-10   top-0">
-        <div className="flex justify-between items-center  s w-full container  mx-auto">
+        <div className="flex justify-between items-center  w-full container  mx-auto">
           <h1 className="font-bold text-xl lg:text-2xl  headline font-black">
-            adeyemi<span className="headline">.</span>
+            adev<span className="headline">.</span>
           </h1>
           <div className="hidden lg:block ">
             <ul className="lg:flex font-bold gap-20">
